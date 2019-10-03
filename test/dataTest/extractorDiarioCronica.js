bf731c7c2a8438bf09bf44fb263aa062baf18f12
jailant <seba0116641295>
2019-10-03 19:54:11 -0300

    Se agrego ecuacion para el Diario Cronica.

---
+++ test/dataTest/extractorDiarioCronica.js
@@ -0,0 +1,11 @@
+module.exports.equationDiarioCronica = {
+    "extractor":"cronica",
+    "eq":{
+        "q": "graduados OR colacion",
+        "siteSearch": "http://diariocronica.com.ar",
+        "cx": "014630729015948810570:xhbqbuvpi2g",
+        "key":"AIzaSyBDpkeAxzpBeFsOsgen9tQkLzpfL_-SWsQ",
+        "siteSearchFilter": "i"
+    },
+    "selectors":[".td-post-content p"]
+};

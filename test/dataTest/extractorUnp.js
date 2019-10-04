42bd54dd0021ae1b38bfba4e57abf6e2ff0e0cec
Lucas <lucasboba@gmail.com>
2019-10-04 09:14:53 -0300

    Se agrego ecuacion para el sitio de la Universidad de la Patagonia

---
+++ test/dataTest/extractorUnp.js
@@ -0,0 +1,11 @@
+module.exports.equationUnp ={
+    "extractor":"unp",
+    "eq":{
+        "q": "graduados + colacion",
+        "siteSearch": "http://www.unp.edu.ar/",
+        "cx": "014630729015948810570:xhbqbuvpi2g",
+        "key":"AIzaSyBDpkeAxzpBeFsOsgen9tQkLzpfL_-SWsQ",
+        "siteSearchFilter": "i"
+    },
+    "selectors":[".tm-article-content p"]
+}

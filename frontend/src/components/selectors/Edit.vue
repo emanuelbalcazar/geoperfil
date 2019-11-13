a82004f9b49ab1af973d980215c6e6c726c8a25b
Emanuel <emanuelbalcazar13@gmail.com>
2019-11-13 14:44:16 -0300

    Cambios menores

---
+++ frontend/src/components/selectors/Edit.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="form-elements">
+    <div class="row">
+      <div class="flex xs12">
+        <va-card :title="text.title">
+          <form>
+            <div class="row">
+              <div class="flex md4 sm6 xs12">
+                <va-input v-model="simple" placeholder="Text Input" />
+              </div>
+            </div>
+          </form>
+        </va-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "edit-selector",
+  components: {},
+  data() {
+    return {
+      text: {
+        title: "Editando Selectores"
+      }
+    };
+  },
+  created() {
+    console.log(this.$route.params);
+  }
+};
+</script>
+
+<style>
+.row.row-inside {
+  max-width: none;
+}
+</style>

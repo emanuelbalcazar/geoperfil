14b6d5c8fc9d0273f41f703af6de160ec183e9ac
Emanuel <emanuelbalcazar13@gmail.com>
2019-11-15 10:38:06 -0300

    Mapa con las sedes en marcadores

--- frontend/src/components/maps/leaflet-maps/LeafletMap.vue
+++ frontend/src/components/maps/leaflet-maps/LeafletMap.vue
@@ -5,32 +5,45 @@
 <script>
 import "leaflet-map";
 import * as Leaflet from "leaflet";
+import axios from "axios";
 
 const initialPosition = [-43.54854811091286, -1148.818359375];
 
 export default {
   name: "leaflet-map",
+  data() {
+    return {
+      campuses: []
+    };
+  },
+  async mounted() {
+    let data = await this.getCampuses();
 
-  mounted() {
-    //    L.Icon.Default.imagePath = 'assets/vendor/leaflet' TODO: make it work with webpack
     Leaflet.Icon.Default.imagePath =
       "https://unpkg.com/leaflet@1.0.3/dist/images";
 
     let map = Leaflet.map(this.$el).setView(initialPosition, 6);
-
-    map.on("click", function(e) {
-      console.log(e.latlng.lat, e.latlng.lng);
-    });
 
     Leaflet.tileLayer("https://{s}.tile.osm.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    Leaflet.marker(initialPosition)
-      .addTo(map)
-      .bindPopup("<b>Marcador personalizado lince</b><hr>")
-      .openPopup();
+    data.forEach(campus => {
+      Leaflet.marker([campus.latitude, campus.longitude], {
+        title: campus.name
+      })
+        .addTo(map)
+        .bindPopup(campus.name)
+        .openPopup();
+    });
+  },
+  methods: {
+    async getCampuses() {
+      return await axios.get("/api/campuses").then(response => {
+        return response.data.data;
+      });
+    }
   }
 };
 </script>

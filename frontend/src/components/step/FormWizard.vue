b99ff8c8a0ac75afe15c16005958eea8b13a82a4
Emanuel <emanuelbalcazar13@gmail.com>
2020-03-11 12:32:28 -0300

    Se agregaron los 3 pasos para completar el formulario

--- frontend/src/components/step/FormWizard.vue
+++ frontend/src/components/step/FormWizard.vue
@@ -60,18 +60,21 @@
         </tab>
 
         <!-- STEP 2 - CAREER -->
-        <tab name="Paso 2" info="Seleccione la carrera" data-vv-scope="step2">
+        <tab name="Paso 2" info="Carrera" data-vv-scope="step2">
           <div id="form-step-2">
             <div class="field">
-              <label class="label">Carrera</label>
+              <label class="label">Seleccione la carrera</label>
               <div class="control">
                 <div class="flex md10 xs12">
                   <va-select
                     :label="text.career"
                     v-model="selectedCareerFromStep"
                     searchable
+                    name="career"
                     textBy="name"
                     :options="careers"
+                    data-vv-scope="step2"
+                    v-validate="'required'"
                   />
                 </div>
                 <p
@@ -84,51 +87,31 @@
         </tab>
 
         <!-- STEP 3 - CAMPUS AND INSTITUTION -->
-        <!-- <tab name="Paso 3" info="Institución">
-            <div id="form-step-3">
-              <div class="field">
-                <label class="label">Where did you hear about us?</label>
-                <div class="control">
-                  <div class="select" name="reference">
-                    <select
-                      v-model="formData.reference"
-                      v-validate="'required'"
-                      name="reference"
-                      data-vv-scope="step3"
-                    >
-                      <option value>Select</option>
-                      <option value="newspaper">Newspaper</option>
-                      <option value="social-media">Social Media</option>
-                      <option value="other">Other</option>
-                    </select>
-                  </div>
+        <tab name="Paso 3" info="Sede institucional">
+          <div id="form-step-3">
+            <div class="field">
+              <label class="label">Seleccione la sede</label>
+              <div class="control">
+                <div class="flex md10 xs12">
+                  <va-select
+                    :label="text.campus"
+                    v-model="selectedCampusFromStep"
+                    searchable
+                    name="campus"
+                    textBy="name"
+                    :options="campuses"
+                    data-vv-scope="step3"
+                    v-validate="'required'"
+                  />
                 </div>
                 <p
                   class="help is-danger"
-                  v-show="errors.has('step3.reference')"
-                >{{ errors.first('step3.reference') }}</p>
-              </div>
-              <div class="field">
-                <div class="control">
-                  <label class="checkbox">
-                    <input
-                      type="checkbox"
-                      name="terms"
-                      v-model="formData.terms"
-                      data-vv-scope="step3"
-                      v-validate="'required'"
-                    />
-                    I agree to the
-                    <a href="#">terms and conditions</a>
-                  </label>
-                </div>
-                <p
-                  class="help is-danger"
-                  v-show="errors.has('step3.terms')"
-                >{{ errors.first('step3.terms') }}</p>
+                  v-show="errors.has('step2.campus')"
+                >{{ errorsText.campus }}</p>
               </div>
             </div>
-        </tab>-->
+          </div>
+        </tab>
       </template>
     </div>
     <br />
@@ -178,15 +161,19 @@ export default {
         noData: "Vacio",
         title: "Profesional Seleccionado",
         editorTitle: "Editor de entidades",
-        career: "Carrera"
+        career: "Carrera",
+        campus: "Sede institucional"
       },
       errorsText: {
         name: "Nombre es requerido",
         surname: "Apellido es requerido",
-        career: "Carrera es requerido"
+        career: "Carrera es requerido",
+        campus: "Sede es requerida"
       },
       selectedCareerFromStep: {},
-      careers: []
+      selectedCampusFromStep: {},
+      careers: [],
+      campuses: []
     };
   },
 
@@ -198,6 +185,7 @@ export default {
   mounted() {
     this.totalTabs = this.tabs.length;
     this.getCareers();
+    this.getCampuses();
   },
 
   methods: {
@@ -239,9 +227,15 @@ export default {
     },
     async getCareers() {
       let response = await axios.get("/api/careers", {
-        params: { page: 1, perPage: 100 }
+        params: { page: 1, perPage: 500 }
       });
       this.careers = response.data.data;
+    },
+    async getCampuses() {
+      let response = await axios.get("/api/campuses", {
+        params: { page: 1, perPage: 500 }
+      });
+      this.campuses = response.data.data;
     }
   }
 };
